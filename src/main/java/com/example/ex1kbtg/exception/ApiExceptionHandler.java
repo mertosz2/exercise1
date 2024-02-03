8b09e67748359e7a671f50af9b2dfd2aa1c9f9a0
Jonathan Chen <sabza178@gmail.com>
2024-02-03 22:51:14 +0700

    adding ApiExceptionHandler Class

---
+++ src/main/java/com/example/ex1kbtg/exception/ApiExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.ex1kbtg.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
+
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseStatusExceptionHandler {
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException e){
+
+        ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND,
+                ZonedDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiExceptionResponse, HttpStatus.NOT_FOUND);
+
+    }
+}
--- src/main/java/com/example/ex1kbtg/exception/ApiExceptionResponse.java
+++ src/main/java/com/example/ex1kbtg/exception/ApiExceptionResponse.java
@@ -1,11 +1,13 @@
 package com.example.ex1kbtg.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
 @AllArgsConstructor
+@Data
 public class ApiExceptionResponse {
 
     private final String message;
